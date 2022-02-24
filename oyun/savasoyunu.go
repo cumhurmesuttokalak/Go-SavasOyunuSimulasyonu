926c0767ad5f4277661d3ce060d4819bd8ac1153
cumhurmessuttokalak <79723267+cumhurmessuttokalak@users.noreply.github.com>
2022-02-24 16:51:09 +0300

    dunyalı ve marslı structları olusturuldu hasar al ve ates et fonksiyonları eklendi gerekli sorgular olusturuldu

--- oyun/savasoyunu.go
+++ oyun/savasoyunu.go
@@ -1,5 +1,9 @@
 package oyun
 
+type ISavascı interface {
+	AtesEt()
+	HasarAl()
+}
 type Savascı struct {
 	Name        string
 	Can         int
@@ -12,4 +16,25 @@ type Dunyali struct {
 type Marsli struct {
 	Savascı
 	UzayGemisi string
+}
+
+func (d *Dunyali) HasarAl() {
+	if d.Can > 0 {
+		d.Can -= 10
+	}
+}
+func (d *Dunyali) AtesEt() {
+	if d.MermiSayisi > 0 {
+		d.MermiSayisi--
+	}
+}
+func (m *Marsli) HasarAl() {
+	if m.Can > 0 {
+		m.Can -= 10
+	}
+}
+func (m *Marsli) AtesEt() {
+	if m.MermiSayisi > 0 {
+		m.MermiSayisi--
+	}
 }
